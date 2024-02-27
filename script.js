7bb2a780b47d9bb6e6a4de54a0afd526de498524
ains95 <ains95xxx@gmail.com>
2024-02-27 20:53:52 +0900

    イベント発火 基礎編

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 let lists=document.getElementsByClassName('pull-down-list')
-console.log(lists);
+console.log(lists)
+const pullDownButton=document.getElementById('lists')
+console.log(pullDownButton)
+window.addEventListener('load',function(){
+    const pullDownButton=document.getElementById('lists')
+})
+    pullDownButton.addEventListener('mousemove',function(){
+        console.log('のる')
+    }) 
+    pullDownButton.addEventListener('mouseout',function(){
+        console.log('外れる')
+    }) 
+    pullDownButton.addEventListener('click',function(){
+        console.log('クリック')      
+     })
